d73adbd6ab37013f662c5844920bce1a05bb8c70
Noah Clark <noahammon00@gmail.com>
2023-09-29 10:44:02 -0600

    learned datalist component

--- app/routes/pokemon.tsx
+++ app/routes/pokemon.tsx
@@ -1,9 +1,23 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link, Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
-    return [
-        {title: 'Pokemon!!'},
-        { name: 'description', content: 'random pokemon'},
-    ]
+  return [
+    { title: "Pokemon!!" },
+    { name: "description", content: "random pokemon" },
+  ];
+};
+
+export default function pokemon() {
+  return (
+    <Form>
+      <h1>why wont u see me</h1>
+      <label htmlFor="pokemon">choose pokemon</label>
+      <input name="pokemon" list="pokemon" />
+        <datalist id="pokemon">
+          <option>pikachu</option>
+          <option>chimchar</option>
+        </datalist>
+    </Form>
+  );
 }
