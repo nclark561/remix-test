5401eb9003a2a26311672327c0c7b00883f56515
Noah Clark <noahammon00@gmail.com>
2023-09-30 22:44:48 -0600

    got pokeapi to create options for all pokemon

--- app/routes/pokemon.tsx
+++ app/routes/pokemon.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link, Form } from "@remix-run/react";
+import { useEffect, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,15 +9,26 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+
 export default function pokemon() {
+  const [pokemon, setPokemon] = useState<any>()
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1292')
+    .then(async(res) => {
+      const resp = await res.json()
+      console.log(resp)
+      setPokemon(resp.results)
+    })
+    .catch(err => console.error(err))
+  }, [])
   return (
     <Form>
       <h1>why wont u see me</h1>
       <label htmlFor="pokemon">choose pokemon</label>
       <input name="pokemon" list="pokemon" />
         <datalist id="pokemon">
-          <option>pikachu</option>
-          <option>chimchar</option>
+          {pokemon?.map((p: any) => <option key={p.name}>{p.name}</option>)}
         </datalist>
     </Form>
   );
